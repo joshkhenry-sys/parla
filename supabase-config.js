3b941aba74ad8201ca5cc7630974d1537a7c0beb
joshkhenry-sys <joshk.henry@gmail.com>
2026-09-17 17:42:35 -0600

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,2 +1,3 @@
-const SUPABASE_URL = "https://tobzuasbijyblusnuics.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://tobzuasbijyblusnuics.supabase.co";
+
 const SUPABASE_ANON_KEY = "sb_publishable_R8B3t39oJIxmhrleX_4Vtg_cuWlRN5j";
